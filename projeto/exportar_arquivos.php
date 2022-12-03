afd59c9c3a6c76052db5b36f476ca922950b58da
Jamallc <weberrocha15@gmail.com>
2022-12-03 16:22:34 -0300

    add exportar_arquivos

---
+++ projeto/exportar_arquivos.php
@@ -0,0 +1,22 @@
+<?php
+
+$dir = dirname(__DIR__);
+
+require_once $dir . "/geral/credentials.php";
+require_once $dir . "/geral/authenticate.php";
+
+$dados = file_get_contents("php://input");
+$dados = json_decode($dados, true);
+
+if (
+	!array_key_exists("TOKEN", $dados) || ($dados["TOKEN"] === '') ||
+	!array_key_exists("PROJETO_ID", $dados) || ($dados["PROJETO_ID"] === '') ||
+	!array_key_exists("Idioma", $dados) || ($dados["Idioma"] === '')
+) {
+	$myObj = new stdClass();
+	$myObj->message = http_response_message(400);
+	$myJSON = json_encode($myObj);
+	die($myJSON);
+}
+
+$id_master = validate_token($dados["TOKEN"])->data->id;
